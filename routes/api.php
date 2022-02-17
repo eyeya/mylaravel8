8d0e55d5548cf7e000801859259de1fa7e187cb3
eyeya <eyephantana2001@hotmail.com>
2022-02-17 17:37:05 +0700

    week13

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BookController;
 use App\Http\Controllers\API\LocationController;
+
+use App\Http\Controllers\API\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,3 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('book', BookController::class);
 
 Route::apiResource('location', LocationController::class);
+Route::post('/sanctum/token', [UserController::class, 'token']);
+
+Route::post('/sanctum/token/register', [UserController::class, 'register']);
